82f6d171640e9332d918e2789a3a2a9deac1c11f
nikiangelov <niki.angelov1@gmail.com>
2020-02-23 12:29:58 +0200

    Setup new project with NextJS and Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+
+const IndexPage: NextPage = () => {
+  const name_Test = 3;
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About {name_Test}</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
