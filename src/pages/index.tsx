cbda60dcecae9b1915095639cd1a41886fb9089b
nikiangelov <niki.angelov1@gmail.com>
2020-02-23 12:57:04 +0200

    Added eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
-import * as React from "react";
-import Link from "next/link";
-import Layout from "../components/Layout";
-import { NextPage } from "next";
+import React from 'react';
 
-const IndexPage: NextPage = () => {
-  const name_Test = 3;
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About {name_Test}</a>
-        </Link>
-      </p>
-    </Layout>
-  );
-};
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { NextPage } from 'next';
+
+class IndexPage extends React.Component<NextPage> {
+  render(): JSX.Element {
+    return (
+      <Layout title="Home | Next.js + TypeScript Example">
+        <h1>Hello Next.js 👋</h1>
+        <p>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </p>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,11 @@
-import { NextPage } from "next";
-import Link from "next/link";
+import React from 'react';
+import { NextPage } from 'next';
+import Link from 'next/link';
 
-import Layout from "../../components/Layout";
-import List from "../../components/List";
-import { User } from "../../interfaces";
-import API from "../../utils/network-client";
+import Layout from '../../components/Layout';
+import List from '../../components/List';
+import { User } from '../../interfaces';
+import API from '../../utils/network-client';
 
 type Props = {
   items: User[];
@@ -31,7 +32,7 @@ WithInitialProps.getInitialProps = async ({ pathname }) => {
   // Example for including initial props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
-  const items: User[] = await API.get("http://localhost:3000/api/users");
+  const items: User[] = await API.get('http://localhost:3000/api/users');
 
   return { items, pathname };
 };
