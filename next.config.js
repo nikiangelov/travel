d5fdf0d059f5258bdf8fbc01533e93c45ff5f39c
nikiangelov <niki.angelov1@gmail.com>
2020-03-01 14:11:35 +0200

    Seting up zeit now env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // next.config.js
-require('./env.js');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
-const webpack = require('webpack');
+require('./env.js');
 
 module.exports = withSass(
   withImages({
@@ -19,9 +18,17 @@ module.exports = withSass(
         exclude: /node_modules/,
         loader: 'graphql-tag/loader',
       });
-      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+      // config.plugins.push(new webpack.EnvironmentPlugin(process.env));
 
       return config;
+    },
+    env: {
+      TEST_VAR: process.env.TEST_VAR,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_PUBLIC_API_KEY: process.env.FIREBASE_PUBLIC_API_KEY,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     },
   }),
 );
--- src/pages/bulgaria/index.tsx
+++ src/pages/bulgaria/index.tsx
@@ -5,21 +5,24 @@ import Link from 'next/link';
 import { useViewerQuery } from '../../apollo/viewer.graphql';
 
 function bulgaria(): ReactElement {
-  const { data } = useViewerQuery();
-  if (!data) return <div>...</div>;
+  const { data, loading } = useViewerQuery();
+  // if (!data) return <div>...</div>;
 
-  const { viewer } = data;
+  const { viewer } = data || {};
   return (
     <Layout>
       <div>bulgaria</div>
-      <div>
-        You&apos;re signed in as {viewer.name} and you&apos;re {viewer.status}{' '}
-        goto{' '}
-        <Link href="/about">
-          <a>static</a>
-        </Link>{' '}
-        page.
-      </div>
+      {loading && <div>Loading...</div>}
+      {!!viewer && (
+        <div>
+          You&apos;re signed in as {viewer.name} and you&apos;re {viewer.status}{' '}
+          goto{' '}
+          <Link href="/about">
+            <a>static</a>
+          </Link>{' '}
+          page.
+        </div>
+      )}
       <img
         src="https://images.unsplash.com/photo-1582466623797-8a7544a402d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
         alt=""
